b93f692833dada69dcf5abcb2bfcea3346f0f598
VayiraSudhan <94919008+VayiraSudhan@users.noreply.github.com>
2023-07-13 14:45:20 +0530

    Update pg4.c

--- day 3/pg4.c
+++ day 3/pg4.c
@@ -2,25 +2,26 @@
 
 int main() {
 
-int number:
+int number;
 
-printf("Enter the number"); scanf("%d",&number);
+printf("Enter the number"); 
+scanf("%d",&number);
 
-int result-seconddigit(number);
+int result=seconddigit(number);
 
 printf("The second last digit of the given number sd 15 %d", number, result);
 
 }
 
-int seconddigit(int number) { if(number<10)
+int seconddigit(int number) {
+    if(number<10)
 {
 return -1;
-
 }
 
 else{
 
-int secondlastdigit (number/10)%10;
+int secondlastdigit=(number/10)%10;
 
 return secondlastdigit;
 }
